b60c118553432ed483aa4f9b2c2f7efb7dbad123
Gizem KELEŞ <gizemkeles28@gmail.com>
2021-01-16 23:45:16 +0300

    16Ocak Ders Sonu

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Kurs kurs1 = new Kurs();
+            kurs1.kursAdi = "C#";
+            kurs1.egitmen = "engin";
+            kurs1.izlenmeOrani = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.kursAdi = "python";
+            kurs2.egitmen = "enginer";
+            kurs2.izlenmeOrani = 78;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.kursAdi = "java";
+            kurs3.egitmen = "zengin";
+            kurs3.izlenmeOrani = 58;
+
+
+            Kurs[] kurslar = new Kurs[] { kurs1, kurs2, kurs3 };
+
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs.kursAdi + ":" + kurs.egitmen);
+            }
+
+
+
+
+
+        }
+    }
+    
+
+    class Kurs
+    {
+        public string kursAdi { get; set; }
+        public string egitmen { get; set; }
+        public int izlenmeOrani { get; set; }
+
+
+      
+
+    }
+
+   
+
+
+
+
+
+
+
+
+}
---
+++ Kampİntro/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Kampİntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            double dolarDun = 7.95;
+            double dolarBugun = 7.95;
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("değişmedi butonu"); 
+            }
+
+            string kurs1 = "Yazılım geliştirici yetiştirme kampı";
+            string kurs2 = "programlamaya başlangıç için temel kurs";
+            string kurs3 = "java";
+
+            string[] kurslar = new string[] { "Yazılım geliştirici yetiştirme kampı", "programlamaya başlangıç için temel kurs", "java" ,"Python","C++"};
+
+            for (int i = 0; i < kurslar.Length ; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine("sayfa sonu - footer");
+
+            Console.WriteLine("for bitti");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
